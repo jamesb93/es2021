c40e34bca6dfd089b40ebf4dfec83145b7a480c2
James Bradbury <me@jamesbradbury.net>
2021-11-13 14:55:10 +0000

    margin signature

--- src/lib/components/CCL.svelte
+++ src/lib/components/CCL.svelte
@@ -9,7 +9,7 @@
         Details of the guests and schedule for the fourth annual Creative Coding Lab Symposium will be announced in early 2021. The symposium will likely be held as an online forum—it will again bring together several guest speakers talking about creative work with audio programming, providing an opportunity for practitioners in creative coding to share and discuss both their technical approaches and their artistic thinking, as well as the way they link the two together.
     
         <div class='signature'>
-            Dr Alex Harker, Director, Creative Coding Lab xoxoxo 🍆🍆🍆🍆🍆🍆
+            Dr Alex Harker, Director, Creative Coding Lab
         </div>
     </div>
 </div>
@@ -39,10 +39,11 @@
 
     .blurb {
         font-size: 1rem;
-        max-width: 48%;
+        max-width: 50%;
     }
 
     .signature {
+        margin-top: 1em;
         font-style: italic;
     }
 </style>

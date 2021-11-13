23c6baa40c191808efef16c8062d4cd054c88624
James Bradbury <me@jamesbradbury.net>
2021-11-13 14:51:42 +0000

    add banner that randomises

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,18 +1,22 @@
-<script>
+<script lang='ts'>
     import '../app.scss';
+    import { shuffle } from '$lib/utils.ts';
     import Title from '$lib/components/Title.svelte';
+
+    let color = [0, 0, 255, 255];
+    const c = shuffle(color).slice(0, 3);
+    // let c = color.map(x => Math.round(Math.random()) * 255);
+    let styleString = `background-color:rgb(${c[0]},${c[1]},${c[2]})`
 </script>
 
-
+<div class="banner" style={styleString} >_</div>
 <Title />
 
 <main>
     <slot />
 </main>
 
-
-
-<style>
+<style lang='scss'>
     main {
         margin-top: 2em;
         margin-bottom: 2em;
@@ -20,13 +24,16 @@
         display: flex;
         flex-direction: column;
         gap: 3em;
-        margin: 0 auto;
-        max-width: 80ch;
+        max-width: $breakpoint;
     }
 
-    @media (min-width: 1024px) {
+    @media (min-width: $breakpoint) {
+        main {
+            margin: 0 auto;
+        }
     }
 
-    @media (max-width: 1024px) {
+    @media (max-width: $breakpoint) {
+
     }
 </style>

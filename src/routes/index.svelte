65104d50546dc5307a257b5be4ec6f651c7fb1b0
James Bradbury <me@jamesbradbury.net>
2021-12-20 16:29:27 +0000

    add back info to main page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,21 @@
 <script>
-    import Info from '$lib/components/Info.svelte';
     import Headliners from '$lib/components/Headliners.svelte';
+    import Footer from '$lib/components/Footer.svelte';
     import AME from '$lib/components/AME.svelte';
     import CCL from '$lib/components/CCL.svelte';
-    import Footer from '$lib/components/Footer.svelte';
 </script>
+
+<div class="info">
+    Electric Spring is a festival of daring electronic music, anchored in the music technology department of the university of huddersfield since 1995. For any inquiry, please write to <a href='mailto:electric.spring@hud.ac.uk'>electric.spring@hud.ac.uk</a>
+</div>
 
 <Headliners />
 <!-- <AME /> -->
-<CCL />
+<!-- <CCL /> -->
 <Footer />
+
+<style>
+    .info {
+        font-size: 1.5rem;
+    }
+</style>

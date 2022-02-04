a2870315527a671373dc5c515ab54dc2bf3e3375
James Bradbury <me@jamesbradbury.net>
2022-02-04 18:37:08 +0000

    update tickets

--- src/routes/line-up.svelte
+++ src/routes/line-up.svelte
@@ -1,43 +1,49 @@
 <h3>Full Schedule</h3>
-All events are free of charge.
+All events are free of charge, however, please prebook tickets so that we can safely manage the number of attendees beforehand.
 
 <div class='container'>
     <div class='day' id='23'>
         <div class='date'>23</div>
         <div class="location">Phipps (7:30-9:30)</div>
-        <div class="act">
-            <div class='act-name'>
-                NAT#
-            </div>
-        </div>
+        <div class="act">NAT#</div>
         <div class="act">NikNak</div>
+        <a class='tickets' href="https://www.eventbrite.co.uk/e/262506874127">TICKETS</a>
     </div>
+    <hr class="vertical-divider">
     <div class='day' id='24'>
         <div class='date'>24</div>
         <div class="location">Phipps (7:30)</div>
         <div class="act">Marie-Jeanne Wyckmans</div>
         <div class="act">Angela Hoyos Gómez</div>
+        <a class='tickets' href="https://www.eventbrite.co.uk/e/262522861947">TICKETS</a>
     </div>
+    <hr class="vertical-divider">
     <div class='day' id='25'>
         <div class='date'>25</div>
         <div class="location">Phipps (7:30-9:30)</div>
         <div class="act">Javier Garavaglia + Claudia Robles-Angel</div>
         <div class="act">Ted Moore + Mark Hanslip</div>
+        <a href="https://www.eventbrite.co.uk/e/262524597137" class="tickets">TICKETS</a>
         <hr>
         <div class="location">Dai Hall (5:30-6:30)</div>
         <div class="act">AME Alvin Lucier Concert (with Scott McLaughlin)</div>
+        <a href="https://www.eventbrite.co.uk/e/262545700257" class="tickets">TICKETS</a>
     </div>
+    <hr class="vertical-divider">
     <div class='day' id='26'>
         <div class='date'>26</div>
         <div class="location">Phipps (7:30-9:30)</div>
         <div class="act">Rian Treanor</div>
         <div class="act">Rachel Gibson</div>
+        <a href="https://www.eventbrite.co.uk/e/262543684227" class="tickets">TICKETS</a>
         <hr>
-        <div class="location">UoH (10am-4pm)</div>
+        <div class="location">UoH G01 (12:00-18:30)</div>
         <div class="act">Creative Code Lab Symposium</div>
+        <a href="https://www.eventbrite.co.uk/e/262548037247" class="tickets">TICKETS</a>
         <hr>
         <div class="location">Dai Hall (9:00-10:30)</div>
         <div class="act">AME Late Night Gig with Joanne Armitage + Eduard Cullenary</div>
+        <a href="https://www.eventbrite.co.uk/e/262553313027" class="tickets">TICKETS</a>
     </div>
 </div>
 
@@ -45,13 +51,17 @@ All events are free of charge.
 
 <h3 id='locations-top'>Locations</h3>
 <div class="legend">
-    <div>(UoH) - University of Huddersfield, Queensgate, Huddersfield HD1 3DH</div>
-    <div class="">(Phipps) - Phipps Hall, University of Huddersfield, Queensgate, Huddersfield HD1 3DH</div>
+    <div>(UoH G01) - G01, University of Huddersfield, signed entrance outside the Richard Steinitz Building</div>
+    <div class="">(Phipps) - Phipps Hall, University of Huddersfield</div>
     <div class="">(Dai Hall) - Dai Hall, Victoria Ln, Huddersfield HD1 2QF</div>
-    <div>(Atrium) - Atrium of the Richard Steinitz Building, University of Huddersfield, Queensgate, Huddersfield HD1 3DH</div>
+    <div>(Atrium) - Atrium of the Richard Steinitz Building, University of Huddersfield</div>
 </div>
 <style lang='scss'>
 
+    .vertical-divider {
+        display: none;
+        visibility: hidden;
+    }
     .location {
         font-size: 0.8rem;
         color: grey;
@@ -71,6 +81,17 @@ All events are free of charge.
         display: grid;
         grid-template-columns: repeat(4, 1fr);
         text-align: left;
+    }
+
+    @media (max-width: 600px) {
+        .container {
+            grid-template-columns: auto;
+            gap: 1em;
+        }
+        .vertical-divider {
+            display: block;
+            visibility: visible;
+        }
     }
 
     .day {

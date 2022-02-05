e1abcfee48b6f2f8d3c1e55e2d9a7d679ff77587
James Bradbury <me@jamesbradbury.net>
2022-02-05 12:26:43 +0000

    add free tickets and artists page

--- src/lib/components/Banner.svelte
+++ src/lib/components/Banner.svelte
@@ -1,15 +1,22 @@
 <nav class="banner" >
     <a class='link' href='/'>home</a>
     <a class='link' href='/line-up'>line-up</a>
+    <a href="/artists" class="link">artists</a>
+    <a target="_blank" href="https://www.eventbrite.co.uk/o/electric-spring-festival-41013064783" class="link">free tickets</a>
 </nav>
 
 <style lang='scss'>
     nav {
+        display: flex;
+        flex-direction: row;
+        gap: 2em;
         background-color: yellow;
         margin-bottom: 1em;
         padding: 0.5em;
+
         .link {
-            font-family: $mono
+            font-family: $mono;
+            font-size: 2rem;
         }
 
         .link:hover {

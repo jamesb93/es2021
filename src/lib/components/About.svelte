c379ae86ffba2fadc32116696de851d9bcfd5b8a
James Bradbury <me@jamesbradbury.net>
2022-02-05 13:05:00 +0000

    make front prettier

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -2,3 +2,10 @@
 <div class="info">
     Electric Spring is a festival of daring electronic music, anchored in the Music Technology Department of the University of Huddersfield since 1995. For any inquiry, please write to <a href='mailto:electric.spring@hud.ac.uk'>electric.spring@hud.ac.uk</a>
 </div>
+
+<style>
+    .info {
+        font-size: 1.75rem;
+        text-align: left;
+    }
+</style>

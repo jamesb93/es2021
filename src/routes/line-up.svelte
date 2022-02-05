9ad3e1f42c23ff736c334fa61bcec5ded7f83e96
James Bradbury <me@jamesbradbury.net>
2022-02-05 12:22:10 +0000

    add blurbs

--- src/routes/line-up.svelte
+++ src/routes/line-up.svelte
@@ -9,6 +9,9 @@
         <div class="location">Phipps (7:30-9:30)</div>
         <div class="act">NAT#</div>
         <div class="act">NikNak</div>
+        <div class="blurb">
+            NAT# performs her intense acid rave howling with psychotic chaos brought to us from the Seychelles via Cumbria. 170 bpm and faster! NikNak starts the evening by a piece for 2 decks and electronics exploring the sonic relationship between the vinyl crackles and the rain.
+        </div>
         <a class='tickets' href="https://www.eventbrite.co.uk/e/262506874127">TICKETS</a>
     </div>
     <hr class="vertical-divider">
@@ -17,6 +20,9 @@
         <div class="location">Phipps (7:30)</div>
         <div class="act">Marie-Jeanne Wyckmans</div>
         <div class="act">Angela Hoyos Gómez</div>
+        <div class="blurb">
+            Marie-Jeanne Wyckmans presents a concert of her multichannel works, including a world premiere and extracts of her latest release on Empreintes DIGITALes. The evening will start with Ángela Hoyos Gómez retro-futuristic immersion into imaginable multilingual and trans-species transmissions.
+        </div>
         <a class='tickets' href="https://www.eventbrite.co.uk/e/262522861947">TICKETS</a>
     </div>
     <hr class="vertical-divider">
@@ -25,10 +31,16 @@
         <div class="location">Phipps (7:30-9:30)</div>
         <div class="act">Javier Garavaglia + Claudia Robles-Angel</div>
         <div class="act">Ted Moore + Mark Hanslip</div>
+        <div class="blurb">
+            Javier Garavaglia and Claudia Robles-Angel bring us a set where their works intertwine, joining multichannel acousmatic sensitivity, interactive mixed music and audiovisual pieces. Opening for them, improvisers Mark Hanslip on saxophone and electronicist Ted Moore appear for the first time as what promises to be an explosive duo.
+        </div>
         <a href="https://www.eventbrite.co.uk/e/262524597137" class="tickets">TICKETS</a>
         <hr>
         <div class="location">Dai Hall (5:30-6:30)</div>
         <div class="act">AME Alvin Lucier Concert (with Scott McLaughlin)</div>
+        <div class="blurb">
+            In a tea-time concert curated by our friends at AME, Scott McLaughlin brings to us a selection of Alvin Lucier (1931-2021) seminal works.
+        </div>
         <a href="https://www.eventbrite.co.uk/e/262545700257" class="tickets">TICKETS</a>
     </div>
     <hr class="vertical-divider">
@@ -37,14 +49,24 @@
         <div class="location">Phipps (7:30-9:30)</div>
         <div class="act">Rian Treanor</div>
         <div class="act">Rachel Gibson</div>
+        <div class="blurb">
+            Rian Treanor gives us to a new site-specific multichannel performance that explores anti-symmetry, oblique digital synthesis, and extreme pattern deformation, forcing
+dynamic contrasts between density, tonality, and locality. Starting the evening, Rachael Gibson will premiere her piece for barytone guitar and electronics.
+        </div>
         <a href="https://www.eventbrite.co.uk/e/262543684227" class="tickets">TICKETS</a>
         <hr>
         <div class="location">UoH G01 (12:00-18:30)</div>
         <div class="act">Creative Code Lab Symposium</div>
+        <div class="blurb">
+            An afternoon of presentations of creative and technical endeavours, featuring commented demonstrations from Francesco Cameli, Ted Moore, Claudia Robles Angel and Javier A. Garavaglia.
+        </div>
         <a href="https://www.eventbrite.co.uk/e/262548037247" class="tickets">TICKETS</a>
         <hr>
         <div class="location">Dai Hall (9:00-10:30)</div>
         <div class="act">AME Late Night Gig with Joanne Armitage + Eduard Cullenary</div>
+        <div class="blurb">
+            A late night gig hosted and curated by our friends at AME, Joanne Armitage presents a live-coding set, followed by a performance by Edward Cullenary.
+        </div>
         <a href="https://www.eventbrite.co.uk/e/262553313027" class="tickets">TICKETS</a>
     </div>
 </div>
@@ -59,7 +81,10 @@
     <div>(Atrium) - Atrium of the Richard Steinitz Building, University of Huddersfield</div>
 </div>
 <style lang='scss'>
+    .blurb {
+        font-size: 0.75rem;
 
+    }
     .vertical-divider {
         display: none;
         visibility: hidden;
